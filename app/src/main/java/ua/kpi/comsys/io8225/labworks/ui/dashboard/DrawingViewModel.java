db52589c3cd57f88744c13151aea182f486ae659
Хана <hanka100h@gmail.com>
2021-02-18 00:37:05 +0200

    Lab 2. Make graphic and diagram drawing in second tab

--- app/src/main/java/ua/kpi/comsys/io8225/labworks/ui/dashboard/DrawingViewModel.java
+++ app/src/main/java/ua/kpi/comsys/io8225/labworks/ui/dashboard/DrawingViewModel.java
@@ -4,11 +4,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class DrawingViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public DrawingViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
